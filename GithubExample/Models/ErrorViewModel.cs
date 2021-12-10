9cc1b29886f5e3d42d6e14b2e068a3301ff609a3
Katherine Pitts <kvp06933@gmail.com>
2021-12-09 22:08:01 -0600

    Add project files.

---
+++ GithubExample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GithubExample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
